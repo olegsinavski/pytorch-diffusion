3428ac5356c24c08fe73f98658d8442b8b5dab15
Oleg Sinavski <sinavski@wayve.ai>
2023-05-03 14:07:09 +0100

    update

---
+++ sandbox.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+PROJECT_NAME=smarts
+PYTHON_VERSION=3.9
+
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+./docker_mlgl/stop_sandbox.sh $PROJECT_NAME
+# Build parent image
+./docker_mlgl/build.sh mlgl_sandbox $PYTHON_VERSION
+docker build -t $PROJECT_NAME $SCRIPT_DIR
+./docker_mlgl/start_sandbox.sh $PROJECT_NAME $SCRIPT_DIR
+
+SANDBOX_IP="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $PROJECT_NAME)"
+ssh docker@$SANDBOX_IP
